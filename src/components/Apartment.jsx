6c37be47c94b9a3cccf5cb286082067d963323d7
Mansour Mbengue <mbgmansour13@gmail.com>
2024-07-08 16:28:55 +0200

    beginning of card style

--- src/components/Apartment.jsx
+++ src/components/Apartment.jsx
@@ -1,11 +1,12 @@
 import { apartmentList } from '../datas/ApartmentList'
 import ApartmentCard from './ApartmentCard'
+import '../styles/Apartment.css'
 
 function Apartment() {
   return (
-    <ul className="apartment-list">
+    <ul>
       {apartmentList.map((apartment) => (
-        <li key={apartment.id}>
+        <li className="apartment-list" key={apartment.id}>
           <ApartmentCard apartment={apartment} />
         </li>
       ))}
