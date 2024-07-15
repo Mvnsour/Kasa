d52fdecc37a5cfc772e65cf0aaf0118cca3d1b3d
Mansour Mbengue <mbgmansour13@gmail.com>
2024-07-15 16:27:02 +0200

    style modification for creation of cards

--- src/components/ApartmentCard.jsx
+++ src/components/ApartmentCard.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 function ApartmentCard({ apartment }) {
   return (
     <React.Fragment>
-      <img
-        className="apartment-cover"
-        src={apartment.cover}
-        alt={`${apartment.title} cover`}
-      />
-      <span className="apartment-title">{apartment.title}</span>)
+      <div className="card-container">
+        <a href="#">
+          <img
+            className="apartment-cover"
+            src={apartment.cover}
+            alt={`${apartment.title} cover`}
+          />
+          <span className="apartment-title">{apartment.title}</span>
+        </a>
+      </div>
     </React.Fragment>
   )
 }
