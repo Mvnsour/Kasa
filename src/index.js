616eadae37fa0de43a5ea0b46f538b183c687e9f
Mansour Mbengue <mbgmansour13@gmail.com>
2024-07-15 15:40:06 +0200

    creation of other paths for the router

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
   },
+  {
+    path: '/apartments',
+    element: <div>Nos appartments</div>,
+  },
+  {
+    path: '/about',
+    element: <div>A propos</div>,
+  },
+  {
+    path: '/404',
+    element: <div>404</div>,
+  },
 ])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
