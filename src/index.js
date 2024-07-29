80b0d86b16f939093bdc81f6f07c388b4fca08df
Mansour Mbengue <mbgmansour13@gmail.com>
2024-07-29 15:55:13 +0200

    same that the last commit

--- src/index.js
+++ src/index.js
@@ -1,48 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'
-import Navbar from './components/Navbar'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ApartmentDetails from './components/ApartmentDetails'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: (
-      <>
-        <Navbar />
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-      </>
-    ),
-  },
-  {
-    path: '/apartments',
-    element: (
-      <>
-        <div>Nos appartments</div>
-      </>
-    ),
-  },
-  {
-    path: '/about',
-    element: (
-      <>
-        <div>A propos</div>
-      </>
-    ),
-  },
-  {
-    path: 'apartment/:id',
-    element: <ApartmentDetails />,
-  },
-])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />{' '}
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
