7c62aa0df350942860683c4aaa0aa45ae49c8639
Mansour Mbengue <mbgmansour13@gmail.com>
2024-07-29 15:31:41 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './styles/App.css'
-import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Main from './components/Main'
 import Apartment from './components/Apartment'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 function App() {
