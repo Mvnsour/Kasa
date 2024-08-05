89ee0616d7748924df0a52a004fe557e37ca39ad
Mansour Mbengue <mbgmansour13@gmail.com>
2024-08-05 18:41:30 +0200

    error page made without style + trying to correct the footer

---
+++ src/pages/Error.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+function Error() {
+  return (
+    <>
+      <h1 className="error-title">404</h1>
+      <p className="error-p">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="home-link">
+        Retourner sur la page d’accueil
+      </Link>
+    </>
+  )
+}
+
+export default Error
