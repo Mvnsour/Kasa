f6d4da537b885c2f8b98af2fbdaec4ccae39cf36
Mansour Mbengue <mbgmansour13@gmail.com>
2024-08-14 22:56:04 +0200

    comment for help me later

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,3 +3,10 @@ import aboutList from '../datas/AboutList'
 function Collapse() {}
 
 export default Collapse
+
+
+{
+  /* mettre une condition si l contenu est du text, on l'affiche, 
+et si c'est un array, on affiche une liste 
+classe sur le parent */
+}
