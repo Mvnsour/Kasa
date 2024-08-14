7fa6bd2361e57a4b771b5873b515f7bd21dfa1d5
Mansour Mbengue <mbgmansour13@gmail.com>
2024-08-14 22:49:03 +0200

    renaming the composant for better readability

---
+++ src/components/TitleAndLocation.jsx
@@ -0,0 +1,10 @@
+function TitleAndLocation({ title, location }) {
+  return (
+    <>
+      <h1 className="apartment-title">{title}</h1>
+      <p className="apartment-p">{location}</p>
+    </>
+  )
+}
+
+export default TitleAndLocation
