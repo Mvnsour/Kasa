e3ab23b700262017cf48b7f8d9322de4f055922a
Mansour Mbengue <mbgmansour13@gmail.com>
2024-08-19 16:23:28 +0200

    display the data of the list in about section and adding style + changing style of the image banner

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,12 +1,26 @@
-import aboutList from '../datas/AboutList'
+import { useState } from 'react'
+import '../styles/Collapse.css'
 
-function Collapse() {}
+function Collapse({ title, content }) {
+  const [showContent, setShowContent] = useState(false)
+  const displayContent = () => {
+    setShowContent(!showContent)
+  }
 
+  return (
+    <div className="collapse-container">
+      <h2 className="collapse-title">
+        {title}
+        <span>
+          <i class="fa-solid fa-chevron-up" onClick={displayContent}></i>
+        </span>
+      </h2>
+      <p className="collapse-content">{content}</p>
+    </div>
+  )
+}
 export default Collapse
 
-
-{
-  /* mettre une condition si l contenu est du text, on l'affiche, 
-et si c'est un array, on affiche une liste 
-classe sur le parent */
-}
+/* mettre une condition si l contenu est du text, 
+ on l'affiche, et si c'est un array, 
+ on affiche une liste classe sur le parent */
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,17 @@
 import AboutBanner from '../assets/about-banner.png'
 import './About.css'
+import Collapse from '../components/Collapse'
+import aboutList from '../datas/AboutList'
 
 function About() {
   return (
     <div className="about-banner-container">
       <img src={AboutBanner} alt="" className="about-banner" />
+      {aboutList.map(({ title, content, index }) => (
+        <div key={index}>
+          <Collapse title={title} content={content} />
+        </div>
+      ))}
     </div>
   )
 }
